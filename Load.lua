e930ec40b865256dec295989b4f54c566ea7ba3a
sparezirt <sonixery@gmail.com>
2025-05-31 23:20:40 +0700

    Create Load.lua

---
+++ Load.lua
@@ -0,0 +1,9 @@
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Krystal Dance V3 Modified by sonixdev, all credits go to Nitro-GT (hemi).")
+task.wait(3)
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/stangithuboffical/selexity/main/Reanimate", true))()
+task.wait(5)
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/krystalDanceV3Modded/refs/heads/patch-1/KrystalDance3.txt"))()
