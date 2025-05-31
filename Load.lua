2ab2f3d55e63507742ff3fe278337abd33f7a9ff
sparezirt <sonixery@gmail.com>
2025-05-31 23:30:44 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -2,8 +2,5 @@ game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Krystal Da
 task.wait(3)
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/stangithuboffical/selexity/main/Reanimate", true))()
-task.wait(5)
-
-
+loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Just-A-Baseplate-Working-Reanimation-39126"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/krystalDanceV3Modded/refs/heads/patch-1/KrystalDance3.txt"))()
