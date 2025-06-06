b10a5774c44a94c51c7a1eefa413be44ca715d83
sparezirt <sonixery@gmail.com>
2025-06-06 17:50:33 +0700

    Update Load.lua

--- Load.lua
+++ Load.lua
@@ -4,4 +4,4 @@ task.wait(3)
 
 loadstring(game:HttpGet("https://rawscripts.net/raw/Just-a-baseplate.-Just-A-Baseplate-Working-Reanimation-39126"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sparezirt/krystalDanceV3Modded/refs/heads/patch-1/KrystalDance3.txt"))()
-game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Script Loaded.")
+game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("Script Loaded.(Y T: sonixery)")
